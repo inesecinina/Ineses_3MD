6440af0bca93c1ec29c52f85526c15890675d0f4
Inese Ciniņa <inese.cinina@gmail.com>
2019-11-24 20:47:35 +0200

    for cikla sastādīšana

--- scripts/3MD.js
+++ scripts/3MD.js
@@ -1,38 +1,26 @@
 console.log('Running 3MD.js');
 
 function main(){ 
-const app = document.querySelector('#container');
-
-function addElement(parent, id, tag = 'div', content = null, classList = null) {
-    const elem = document.createElement(tag);
-    elem.id = id;
-    if (content) elem.innerText = content;
-    if (classList) elem.classList.add(...classList);
-    parent.appendChild(elem);
-    return elem;
-}
-
-function addElemObj(parent, obj) {
-    const elem = document.createElement(obj.tag);
-    elem.id = obj.id;
-    elem.innerText = obj.innerText;
-    parent.appendChild(elem);
-    return elem;
-}
+const container = document.querySelector('#container');
 
 
-   for (let i = 1; i < 100; i++) {
-    const miniDiv = addElement(app, "mini" + i, "div", "My text " + i);
-    if(i%3===0) {
-        elem.classList.add("fizz");
+   for (let i = 1; i <= 100; i++) {
+    const miniDiv = document.createElement('div');
+    miniDiv.id = "miniDiv"+i;
+    if(i%3===0 && i%5===0) {
+        miniDiv.innerText = i+" = FizzBuzz";
+        miniDiv.classList.add("fizzbuzz");
     } else if (i%5===0){
-        elem.classList.add("buzz");
-    } else if (i%3===0 && i%5===0) {
-        elem.classList.add("fizzbuzz");
+        miniDiv.innerText = i+" = Buzz";
+        miniDiv.classList.add("buzz");
+    } else if (i%3===0 ) {
+        miniDiv.innerText = i+" = Fizz";
+        miniDiv.classList.add("fizz");
     } else {
-        elem.classList.add("regular");
+        miniDiv.innerText = i;
+        miniDiv.classList.add("regular");
     }
-
-    addElemObj(miniDiv, { id: "ob" + i, tag: "div", innerText: "Text" + i }) 
+    container.appendChild(miniDiv);
 }
 }
+main();
