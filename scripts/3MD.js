b72b1a7b4fc148d25a410286d9a9cb4aeebbfedd
Inese Ciniņa <inese.cinina@gmail.com>
2019-11-25 21:04:48 +0200

    ievadlauki un pogas

--- scripts/3MD.js
+++ scripts/3MD.js
@@ -1,19 +1,31 @@
 console.log('Running 3MD.js');
 
-function main(){ 
+
+const generators = document.getElementById('generator'); 
+const cleaneris = document.getElementById('cleaner'); 
+const start = document.getElementById('starts');
+const stop = document.getElementById('stops');
+const fizzs = document.getElementById('fizz');
+const buzzs = document.getElementById('buzz'); 
+
 const container = document.querySelector('#container');
 
+generators.onclick = () => {
+    const starts = parseFloat(start.value);
+    const stops = parseFloat(stop.value);
+    const fizz = parseFloat(fizzs.value);
+    const buzz = parseFloat(buzzs.value);
 
-   for (let i = 1; i <= 100; i++) {
+   for (let i = starts; i <= stops; i++) {
     const miniDiv = document.createElement('div');
     miniDiv.id = "miniDiv"+i;
-    if(i%3===0 && i%5===0) {
+    if(i%fizz === 0  && i%buzz === 0) {
         miniDiv.innerText = i+"  FizzBuzz";
         miniDiv.classList.add("fizzbuzz");
-    } else if (i%5===0){
+    } else if (i%buzz === 0){
         miniDiv.innerText = i+"  Buzz";
         miniDiv.classList.add("buzz");
-    } else if (i%3===0 ) {
+    } else if (i%fizz === 0 ) {
         miniDiv.innerText = i+"  Fizz";
         miniDiv.classList.add("fizz");
     } else {
@@ -21,6 +33,13 @@ const container = document.querySelector('#container');
         miniDiv.classList.add("regular");
     }
     container.appendChild(miniDiv);
+} 
+generators.disabled = true;
+};
+cleaneris.onclick = () => {
+    let miniDiv = document.getElementById("container");
+    while (miniDiv.firstChild) {
+      miniDiv.removeChild(miniDiv.firstChild);
+    }
+    generators.disabled = false;
 }
-}
-main();
